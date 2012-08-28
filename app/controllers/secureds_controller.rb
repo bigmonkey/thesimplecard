42db5fe139fcfde408b214db02fcbc686a6983c2
cw <conway@xerpi.com>
2012-08-29 00:32:31 +0200

    complete secured card site heroku ready

--- app/controllers/secureds_controller.rb
+++ app/controllers/secureds_controller.rb
@@ -9,6 +9,8 @@ class SecuredsController < ApplicationController
       max_balance = 3000
       max_duration = 48
       duration_range = (1..48)
+      pur_balance_range = (0..max_balance)
+      cash_balance_range = (0..max_balance)
 
       @pur_balance =  400.0 #revolving purchase balance
       @cash_balance = 100.0 #cash balance balance
@@ -18,7 +20,7 @@ class SecuredsController < ApplicationController
       # if params hold different criteria reset criteria
       if !params[:criteria].nil? then # do params exist
         if (params[:criteria][:PurBal].is_a? String) && (params[:criteria][:CashBal].is_a? String) && (params[:criteria][:Duration].is_a? String) then # are the params strings, i.e. will to_i work
-          if pur_balance_range === params[:criteria][:PurBal].to_i && @cash_balance_range === params[:criteria][:CashBal].to_i && @duration_range === params[:criteria][:Duration].to_i then  # check that numbers are in range
+          if pur_balance_range === params[:criteria][:PurBal].to_i && cash_balance_range === params[:criteria][:CashBal].to_i && duration_range === params[:criteria][:Duration].to_i then  # check that numbers are in range
       		  @pur_balance = params[:criteria][:PurBal].to_i
         		@cash_balance = params[:criteria][:CashBal].to_i
         		@duration = params[:criteria][:Duration].to_i
